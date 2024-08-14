74e8577e6e6cfeaaa0832075db0fbe81b01dc596
maalikwatson <watsmaa@outlook.com>
2024-08-13 21:04:37 -0400

    Added first and last name prompts, then changed local variable 'retirementAge' to 65 from 45, respectively.

--- ConsoleApp.UserInputDemo/Program.cs
+++ ConsoleApp.UserInputDemo/Program.cs
@@ -1,11 +1,15 @@
 ﻿// Declare variables
-string name = string.Empty;
+string firstName = string.Empty;
+string lastName = string.Empty;
 int age = 0;
-int retirementAge = 45;
+int retirementAge = 65;
 
 // Prompt user for input
-Console.WriteLine("Please enter your name: ");
-name = Console.ReadLine();
+Console.WriteLine("Please enter your first name: ");
+firstName = Console.ReadLine();
+
+Console.WriteLine("Please enter your last name: ");
+lastName = Console.ReadLine();
 
 Console.WriteLine("Please enter your age: ");
 age = Convert.ToInt32(Console.ReadLine());
@@ -14,6 +18,6 @@ age = Convert.ToInt32(Console.ReadLine());
 int workingYearsRemaining = retirementAge - age;
 
 // Output results to user
-Console.WriteLine($"Full name: {name}");
+Console.WriteLine($"Full name: {firstName} {lastName}");
 Console.WriteLine($"Age: {age}");
 Console.WriteLine($"Working years remaining: {workingYearsRemaining}");
