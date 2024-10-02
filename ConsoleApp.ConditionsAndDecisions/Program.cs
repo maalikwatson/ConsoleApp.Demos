ffb62e5249de2547b5d656776257ee661a0ef81e
watsmaa <watsmaa@outlook.com>
2024-10-02 11:13:36 -0400

    Switch statement added.

--- ConsoleApp.ConditionsAndDecisions/Program.cs
+++ ConsoleApp.ConditionsAndDecisions/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Prompt for input
 
 Console.Write("Please enter student's grade: ");
+// 'grade' is a global variable / global scope
 int grade = Convert.ToInt32(Console.ReadLine());
 
 // Simple If...Else...statement -- Decide to print pass or fail based on input
@@ -110,6 +111,7 @@ else if (dayOfWeek == 7)
 }
 */
 
+Console.Write("Please enter the day of the week: ");
 int dayOfWeek = Convert.ToInt32(Console.ReadLine());
 switch (dayOfWeek)
 {
@@ -117,6 +119,29 @@ switch (dayOfWeek)
         Console.WriteLine("Sunday");
         // more code
         // more code
+        break;
+    case 2:
+        Console.WriteLine("Monday");
+        break;
+    case 3:
+        Console.WriteLine("Tuesday");
+        break;
+    case 4:
+        Console.WriteLine("Wednesday");
+        Console.WriteLine("Hump Day!");
+        break;
+    case 5:
+        Console.WriteLine("Thursday");
+        break;
+    case 6:
+        Console.WriteLine("Friday");
+        break;
+    case 7:
+        Console.WriteLine("Saturday");
+        Console.WriteLine("Beach Day!");
+        break;
+    default:
+        Console.WriteLine("Invalid Day Number Entered");
         break;
 }
 
